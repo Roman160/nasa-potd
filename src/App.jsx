948d4d23c72d9d69e6446073652abd2385f71b1b
roman <roman.simmons@gmail.com>
2022-01-11 09:17:44 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NasaPhoto from "./components/NasaPhoto";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+        <NasaPhoto />    
+        </div>   
+  );
+}
